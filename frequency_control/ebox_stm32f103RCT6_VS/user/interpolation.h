bcbeb92099f88ed7d5f693a3de3631510cfe9428
pidan1231239 <850576570@qq.com>
2017-03-30 21:13:16 +0800

    增加线性插值的越界预测

--- frequency_control/ebox_stm32f103RCT6_VS/user/interpolation.h
+++ frequency_control/ebox_stm32f103RCT6_VS/user/interpolation.h
@@ -18,11 +18,22 @@ namespace sky {
 
 	double Interpolation::operator()(double x)
 	{
-		for (int i = 0; i < length; i++)
+		if (x < dataX[0])
 		{
-			if (x >= dataX[i] && x <= dataX[i + 1])
+			return (x - dataX[0]) / (dataX[1] - dataX[0])*(dataY[1] - dataY[0]) + dataY[0];
+		}
+		else if (x > dataX[length - 1]) {
+			return (x - dataX[length - 1]) / (dataX[length - 1] - dataX[length - 2]) \
+				*(dataY[length - 1] - dataY[length - 2]) + dataY[length - 1];
+		}
+		else
+		{
+			for (int i = 0; i < length; i++)
 			{
-				return (x - dataX[i]) / (dataX[i + 1] - dataX[i])*(dataY[i + 1] - dataY[i]) + dataY[i];
+				if (x >= dataX[i] && x <= dataX[i + 1])
+				{
+					return (x - dataX[i]) / (dataX[i + 1] - dataX[i])*(dataY[i + 1] - dataY[i]) + dataY[i];
+				}
 			}
 		}
 		return 0;
